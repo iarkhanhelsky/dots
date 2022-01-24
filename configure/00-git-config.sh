5f7494592ddfa6d072d6e0f72c7128936cb8e422
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-01-24 21:18:07 +0200

    Add git-aliases script

---
+++ configure/00-git-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+GIT_CONFIG="git config"
+
+echo ":: Set git aliases"
+
+# Remove all aliases
+for al in $($GIT_CONFIG --get-regexp '^alias\.' | awk '{print $1}'); do
+    echo "  Unset: $al"
+    $GIT_CONFIG --unset "$al"
+done
+
+# Update with new set of aliases
+$GIT_CONFIG alias.slog "log --color --graph --pretty=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit"
+$GIT_CONFIG alias.glog "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+echo ":: Updated"
+$GIT_CONFIG --get-regexp '^alias\.'  | awk '{print "   - " $1}'
