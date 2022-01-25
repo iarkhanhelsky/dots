e319529cdc9a884fcda5a1761e22cb5af4046f50
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-01-25 10:40:36 +0000

    git: fix slog template

--- configure/00-git-config.sh
+++ configure/00-git-config.sh
@@ -10,7 +10,7 @@ for al in $($GIT_CONFIG --get-regexp '^alias\.' | awk '{print $1}'); do
 done
 
 # Update with new set of aliases
-$GIT_CONFIG alias.slog "log --color --graph --pretty=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an% <%ae>C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit"
+$GIT_CONFIG alias.slog "log --color --graph --pretty=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an <%ae>%C(reset)%C(bold yellow)%d%C(reset)' --abbrev-commit"
 $GIT_CONFIG alias.glog "log --graph --oneline --branches"
 
 echo ":: Updated"
