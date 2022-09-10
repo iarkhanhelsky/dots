813ee598261d41957096b8681fe2d1aedb0ec087
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-09-10 18:55:32 +0300

    fix(rc-darwin): use gls opts only if gls present

--- rc-darwin/zshrc.d/00-aliases-darwin.sh
+++ rc-darwin/zshrc.d/00-aliases-darwin.sh
@@ -1,2 +1,4 @@
-alias ls='gls -lh --group-directories-first --color=always'
+if command gls 1>/dev/null 2>/dev/null; then
+    alias ls='gls -lh --group-directories-first --color=always'
+fi
 alias lsa='ls -a'
